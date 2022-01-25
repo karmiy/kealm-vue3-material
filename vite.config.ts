3e6bb138e2befe204a0f50cc6c010fc32a4d8964
karmiy <532981481@qq.com>
2022-01-25 23:37:25 +0800

    feat: vite + vue3

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
         alias: {
             '@': resolve(__dirname, 'src'),
         },
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     },
     server: {
         host: '0.0.0.0',
